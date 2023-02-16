ae40d18932d9512115527de18b53221202e789bb
felis <felis.blue@gmail.com>
2023-02-16 21:25:50 +0100

    Add java docs

--- app/controllers/MainController.java
+++ app/controllers/MainController.java
@@ -3,8 +3,17 @@ package controllers;
 import play.mvc.Controller;
 import play.mvc.Result;
 
+/**
+ * Controller handling the requests for the index page, as well as the yarn list
+ * and yarn items
+ */
 public class MainController extends Controller {
 
+    /**
+     * The index page
+     *
+     * @return the index template rendered to HTML
+     */
     public Result index() {
         return ok(views.html.index.render());
     }
