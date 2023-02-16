f7690132459617a9524759c6aa1654ed3d4383ce
felis <felis.blue@gmail.com>
2023-02-16 21:51:55 +0100

    Add the yarn list and item endpoints

--- app/controllers/MainController.java
+++ app/controllers/MainController.java
@@ -1,7 +1,12 @@
 package controllers;
 
+import java.util.Optional;
+
+import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
+
+import model.Yarn;
 
 /**
  * Controller handling the requests for the index page, as well as the yarn list
@@ -18,4 +23,24 @@ public class MainController extends Controller {
         return ok(views.html.index.render());
     }
 
+    /**
+     * The yarn list
+     *
+     * @return the list of yarn as JSON
+     */
+    public Result list() {
+        return ok(Json.toJson(Yarn.getAll()));
+    }
+
+    /**
+     * A yarn item
+     *
+     * @param id
+     *           the id of the requested item
+     * @return the yarn item with the given id as JSON
+     */
+    public Result item(final String id) {
+        Optional<Yarn> yarn = Yarn.getById(id);
+        return yarn.map(y -> ok(Json.toJson(y))).orElse(notFound("No such id"));
+    }
 }
